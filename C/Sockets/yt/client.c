ccc0a989a2a08d3a4adff8fb1f2460ada27c7877
Shivakumarbharath <shivakumar.bharath@gmail.com>
2021-10-01 18:12:36 +0530

    Tutorial updates

---
+++ C/Sockets/yt/client.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>		//used to store the information about a given host
+
+void
+error (const char *msg)
+{
+  perror (msg);			//inbuilt function that outputs with stderr with error number
+  exit (1);
+}
+
+int
+main (int argc, char *argv[])
+{				//to take the arguments from the command line
+
+
+  int sockfd, portno, n;
+  char buffer[255];
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+
+  if (argc < 3)
+    {
+      printf
+	("stderr  Port number Ip address not provided . Program Terminated\nUsage Filename <IP Address> <Port Number>");
+      exit (-1);
+
+    }
+
+
+  portno = atoi (argv[2]);
+  sockfd = socket (AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    {
+      error ("Error opening a socket!!");
+    }
+
+  server = gethostbyname (argv[1]);	//get the host name
+  if (server == NULL)
+    error ("No such host \nTry Again with the valid host");
+  bzero ((char *) &serv_addr, sizeof (serv_addr));	//clears any data whatever it is refereced to
+  serv_addr.sin_family = AF_INET;
+  bcopy ((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr,
+	 server->h_length);
+  serv_addr.sin_port = htons (portno);
+  if (connect (sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) <
+      0)
+    error ("Connection failed");
+  while (1)
+    {
+      bzero (buffer, 255);
+      fgets (buffer, 255, stdin);
+      n = write (sockfd, buffer, strlen (buffer));
+      if (n < 0)
+	error ("Wrinting Failed");
+      bzero (buffer, 255);
+      n = read (sockfd, buffer, 255);
+      if (n < 0)
+	error ("Reading Failed");
+      printf ("Server: %s", buffer);
+      int i = strncmp ("bye", buffer, 3);
+      if (i == 0)
+	break;
+    }
+
+  return 0;
+}
--- C/Sockets/yt/server.c
+++ C/Sockets/yt/server.c
@@ -6,25 +6,68 @@
 #include <string.h>
 #include <unistd.h>
 
-void error(const char *msg){
-perror(msg);//inbuilt function that outputs with stderr with error number
-exit(1);
+void
+error (const char *msg)
+{
+  perror (msg);			//inbuilt function that outputs with stderr with error number
+  exit (1);
 }
 
-int main(int argc,char *argv[]){//to take the arguments from the command line
+int
+main (int argc, char *argv[])
+{				//to take the arguments from the command line
 
-if (argc<2){
-fprintf("stderr  Port number not provided . Program Terminated");
-exit(0);
+  if (argc < 2)
+    {
+      printf ("stderr  Port number not provided . Program Terminated");
+      exit (-1);
+
+    }
+  int sockfd, newsockfd, portno, n;
+  char buffer[255];
+  struct sockaddr_in serv_addr, client_addr;	//internet address in netinet
+  socklen_t client_len;		//
+
+  sockfd = socket (AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    {
+      error ("Error opening a socket!!");
+    }
+  bzero ((char *) &serv_addr, sizeof (serv_addr));	//clears any data whatever it is refereced to
+  portno = atoi (argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons (portno);
+  if (bind (sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
+    error ("Binding Failed");
+  listen (sockfd, 5);
+  client_len = sizeof (client_addr);
+  newsockfd = accept (sockfd, (struct sockaddr *) &client_addr, &client_len);
+
+  if (newsockfd < 0)
+    error ("Error while accepting");
+
+  while (1)
+    {
+
+      bzero (buffer, 255);
+      n = read (newsockfd, buffer, sizeof (buffer));
+      if (n < 0)
+	error ("Error while reading");
+      printf ("Client :%s\n", buffer);
+      bzero (buffer, 255);
+      fgets (buffer, 255, stdin);
+      n = write (newsockfd, buffer, strlen (buffer));
+      if (n < 0)
+	error ("Error on writing");
+
+      int i = strncmp ("Bye", buffer, 3);
+      if (i == 0)
+	break;
+    }
+
+  close (newsockfd);
+  close (sockfd);
+  return 0;
 
 }
-int sockfd,newsockfd,portno,n;
-char buffer[255];
-struct sockaddr_in serv_addr,client_addr;
-socklen_t client_len;
-
-
-return 0;
-
-}
-
